651f6fedf03d45dae666924469878af041482155
brenton26-afk <77509555+brenton26-afk@users.noreply.github.com>
2023-04-18 08:17:15 -0400

    Create main.java

---
+++ AP_Study/main.java
@@ -0,0 +1,13 @@
+/* This is to call and test all of the skills I need to practice */
+
+// Call everything in here
+public class MyConsole {
+
+  // Overriding methods
+  
+  // Superclass and Subclass
+  //Dog fido = new UnderDog();
+
+
+
+}
